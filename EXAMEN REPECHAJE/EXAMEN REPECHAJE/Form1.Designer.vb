d7b3918ab838d4aa6f6d6cc1577423af72c931be
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2022-08-22 15:02:14 -0400

    Examen de Areas Geometricas

---
+++ EXAMEN REPECHAJE/EXAMEN REPECHAJE/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.MENUToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RESPUESTA2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MENUToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(561, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
+        '
+        'MENUToolStripMenuItem
+        '
+        Me.MENUToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RESPUESTA1ToolStripMenuItem, Me.RESPUESTA2ToolStripMenuItem})
+        Me.MENUToolStripMenuItem.Name = "MENUToolStripMenuItem"
+        Me.MENUToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.MENUToolStripMenuItem.Text = "MENU"
+        '
+        'RESPUESTA1ToolStripMenuItem
+        '
+        Me.RESPUESTA1ToolStripMenuItem.Name = "RESPUESTA1ToolStripMenuItem"
+        Me.RESPUESTA1ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTA1ToolStripMenuItem.Text = "RESPUESTA 1"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(182, 128)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(182, 234)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(179, 88)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "LADO:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(179, 210)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "RESULTADO:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(304, 128)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(301, 109)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "ALTURA:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(179, 109)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "RADIO:"
+        '
+        'RESPUESTA2ToolStripMenuItem
+        '
+        Me.RESPUESTA2ToolStripMenuItem.Name = "RESPUESTA2ToolStripMenuItem"
+        Me.RESPUESTA2ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTA2ToolStripMenuItem.Text = "RESPUESTA 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(561, 349)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MENUToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RESPUESTA1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RESPUESTA2ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ EXAMEN REPECHAJE/EXAMEN REPECHAJE/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Public Function A_Cuadrado(l As Double) As Double
+        Dim Ac As Double = l ^ 2
+        Return Ac
+    End Function
+
+    Public Function A_Triangulo(b As Double, h As Double) As Double
+        Dim At As Double = b * h / 2
+        Return At
+    End Function
+
+    Public Function Dif_1(l As Double) As Double
+        Dim Dif1 As Double = A_Cuadrado(l) - A_Triangulo(l, l / 2)
+        Return Dif1
+    End Function
+
+    Private Sub RESPUESTA1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTA1ToolStripMenuItem.Click
+        TextBox2.Text = Dif_1(TextBox1.Text)
+    End Sub
+
+
+
+    Public Function A_Rectangulo(b As Double, h As Double) As Double
+        Dim At As Double = b * h
+        Return At
+    End Function
+
+    Public Function A_Circunfrencia(r As Double) As Double
+        Dim Ac As Double = Math.PI * r ^ 2
+        Return Ac
+    End Function
+
+    Public Function Peri_Cir(r As Double) As Double
+        Dim PeriCir As Double
+        PeriCir = 2 * Math.PI * r
+        Return PeriCir
+    End Function
+
+    Public Function Sum_4(r As Double, h As Double) As Double
+        Dim Sum4 As Double = 2 * A_Circunfrencia(r) + A_Rectangulo(Peri_Cir(r), h)
+        Return Sum4
+    End Function
+
+    Private Sub RESPUESTA2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTA2ToolStripMenuItem.Click
+        TextBox2.Text = Sum_4(TextBox1.Text, TextBox3.Text)
+    End Sub
+End Class
