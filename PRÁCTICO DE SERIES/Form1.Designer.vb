556a0a058c2089047db2c010aedebd05d2174da9
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2022-08-22 14:30:17 -0400

    Practico de Series

---
+++ PRÁCTICO DE SERIES/Form1.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.PRÁCTICODESERIESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE3ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE4ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE5ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE6ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE7ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE8ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE9ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SERIE10ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.SERIE11ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PRÁCTICODESERIESToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(627, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'PRÁCTICODESERIESToolStripMenuItem
+        '
+        Me.PRÁCTICODESERIESToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SERIE1ToolStripMenuItem, Me.SERIE2ToolStripMenuItem, Me.SERIE3ToolStripMenuItem, Me.SERIE4ToolStripMenuItem, Me.SERIE5ToolStripMenuItem, Me.SERIE6ToolStripMenuItem, Me.SERIE7ToolStripMenuItem, Me.SERIE8ToolStripMenuItem, Me.SERIE9ToolStripMenuItem, Me.SERIE10ToolStripMenuItem, Me.SERIE11ToolStripMenuItem})
+        Me.PRÁCTICODESERIESToolStripMenuItem.Name = "PRÁCTICODESERIESToolStripMenuItem"
+        Me.PRÁCTICODESERIESToolStripMenuItem.Size = New System.Drawing.Size(129, 20)
+        Me.PRÁCTICODESERIESToolStripMenuItem.Text = "PRÁCTICO DE SERIES"
+        '
+        'SERIE1ToolStripMenuItem
+        '
+        Me.SERIE1ToolStripMenuItem.Name = "SERIE1ToolStripMenuItem"
+        Me.SERIE1ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE1ToolStripMenuItem.Text = "SERIE 1"
+        '
+        'SERIE2ToolStripMenuItem
+        '
+        Me.SERIE2ToolStripMenuItem.Name = "SERIE2ToolStripMenuItem"
+        Me.SERIE2ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE2ToolStripMenuItem.Text = "SERIE 2"
+        '
+        'SERIE3ToolStripMenuItem
+        '
+        Me.SERIE3ToolStripMenuItem.Name = "SERIE3ToolStripMenuItem"
+        Me.SERIE3ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE3ToolStripMenuItem.Text = "SERIE 3"
+        '
+        'SERIE4ToolStripMenuItem
+        '
+        Me.SERIE4ToolStripMenuItem.Name = "SERIE4ToolStripMenuItem"
+        Me.SERIE4ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE4ToolStripMenuItem.Text = "SERIE 4"
+        '
+        'SERIE5ToolStripMenuItem
+        '
+        Me.SERIE5ToolStripMenuItem.Name = "SERIE5ToolStripMenuItem"
+        Me.SERIE5ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE5ToolStripMenuItem.Text = "SERIE 5"
+        '
+        'SERIE6ToolStripMenuItem
+        '
+        Me.SERIE6ToolStripMenuItem.Name = "SERIE6ToolStripMenuItem"
+        Me.SERIE6ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE6ToolStripMenuItem.Text = "SERIE 6"
+        '
+        'SERIE7ToolStripMenuItem
+        '
+        Me.SERIE7ToolStripMenuItem.Name = "SERIE7ToolStripMenuItem"
+        Me.SERIE7ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE7ToolStripMenuItem.Text = "SERIE 7"
+        '
+        'SERIE8ToolStripMenuItem
+        '
+        Me.SERIE8ToolStripMenuItem.Name = "SERIE8ToolStripMenuItem"
+        Me.SERIE8ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE8ToolStripMenuItem.Text = "SERIE 8"
+        '
+        'SERIE9ToolStripMenuItem
+        '
+        Me.SERIE9ToolStripMenuItem.Name = "SERIE9ToolStripMenuItem"
+        Me.SERIE9ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE9ToolStripMenuItem.Text = "SERIE 9"
+        '
+        'SERIE10ToolStripMenuItem
+        '
+        Me.SERIE10ToolStripMenuItem.Name = "SERIE10ToolStripMenuItem"
+        Me.SERIE10ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE10ToolStripMenuItem.Text = "SERIE 10"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(43, 181)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 19)
+        Me.Label6.TabIndex = 34
+        Me.Label6.Text = "RESULTADO:"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BackColor = System.Drawing.SystemColors.Window
+        Me.TextBox7.Location = New System.Drawing.Point(47, 263)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(538, 20)
+        Me.TextBox7.TabIndex = 33
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(47, 220)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(283, 20)
+        Me.TextBox6.TabIndex = 32
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(383, 103)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox4.TabIndex = 31
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(271, 103)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox3.TabIndex = 30
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(159, 103)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox2.TabIndex = 29
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(47, 103)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox1.TabIndex = 28
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(495, 103)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(90, 20)
+        Me.TextBox5.TabIndex = 35
+        '
+        'SERIE11ToolStripMenuItem
+        '
+        Me.SERIE11ToolStripMenuItem.Name = "SERIE11ToolStripMenuItem"
+        Me.SERIE11ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SERIE11ToolStripMenuItem.Text = "SERIE 11"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(627, 354)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(643, 393)
+        Me.MinimumSize = New System.Drawing.Size(643, 393)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents PRÁCTICODESERIESToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE3ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE4ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE5ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE6ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE7ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE8ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE9ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SERIE10ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents SERIE11ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ PRÁCTICO DE SERIES/Form1.vb
@@ -0,0 +1,381 @@
+﻿Public Class Form1
+    Public Function Serie_Factorial(n As Integer) As Double
+        Dim i As Integer
+        Dim f As Double
+        If (n > 0) Then
+            f = 1
+            For i = n To 1 Step -1
+                f = f * i
+            Next
+        Else
+            f = 1
+        End If
+        Return f
+    End Function
+
+    Public Function Serie_1(n As Integer, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, m As Double
+        f = 0
+        m = 10
+        For i = 1 To n
+            t = Math.Log10(m) / (vi + (i - 1) * r)
+            m = m * 10
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie1_Tex(n As Integer, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim m As Double
+        f = " F = "
+        m = 10
+        For i = 1 To n
+            t = "Log10(" + Str(m) + ")/(" + Str(vi + (i - 1) * r) + ") + "
+            m = m * 10
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_2(n As Integer, vi As Double) As Double
+        Dim i As Integer
+        Dim f, t As Double
+        f = 0
+        For i = 1 To n
+            t = vi
+            vi = vi - i
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie2_Tex(n As Integer, vi As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        f = " F = "
+        For i = 1 To n
+            t = Str(vi) + " + "
+            vi = vi - i
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_3(n As Integer, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, s, vi2, r2 As Double
+        f = 0
+        vi2 = 10 : r2 = 2
+        For i = 1 To n
+            s = vi2 - (i - 1) * r2
+            t = (vi + (i - 1) * r) / Serie_Factorial(s)
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie3_Tex(n As Integer, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim s, vi2, r2 As Double
+        f = " F = "
+        vi2 = 10 : r2 = 2
+        For i = 1 To n
+            s = vi2 - (i - 1) * r2
+            t = Str(vi + (i - 1) * r) + "/" + Str(s) + "! + "
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_4(n As Integer, x As Double, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, s, vi2, r2 As Double
+        f = 0
+        vi2 = 2 : r2 = 2
+        For i = 1 To n
+            s = vi2 + (i - 1) * r2
+            t = x ^ (vi + (i - 1) * r) / Serie_Factorial(s)
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie4_Tex(n As Integer, x As Double, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim s, vi2, r2 As Double
+        f = " F = "
+        vi2 = 2 : r2 = 2
+        For i = 1 To n
+            s = vi2 + (i - 1) * r2
+            t = "(x^" + Str(vi + (i - 1) * r) + ")/" + Str(s) + "! + "
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_5(n As Integer, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, z, x, y As Double
+        f = 0
+        x = -1 : y = 1
+        For i = 1 To n
+            z = x + y : x = y : y = z
+            t = z ^ (1 / (vi + (i - 1) * r))
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie5_Tex(n As Integer, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim z, x, y As Double
+        f = " F = "
+        x = -1 : y = 1
+        For i = 1 To n
+            z = x + y : x = y : y = z
+            t = Str(z) + " ^ (1/" + Str(vi + (i - 1) * r) + ") + "
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_6(n As Integer, vi As Double, r As Double, vi1 As Double, r1 As Double) As Double
+        Dim i As Integer
+        Dim f As Double
+        f = 0
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f = f + (Math.Cos(vi1 + (i - 1) * r1) ^ (1 / (vi + (i - 1) * r)))
+            Else
+                f = f - (Math.Cos(vi1 + (i - 1) * r1) ^ (1 / (vi + (i - 1) * r)))
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie6_Tex(n As Integer, vi As Double, r As Double, vi1 As Double, r1 As Double) As String
+        Dim i As Integer
+        Dim f As String
+        f = " F = "
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f += "Cos(" + Str(vi1 + (i - 1) * r1) + ") ^ (1/" + Str(vi + (i - 1) * r) + ") - "
+            Else
+                f += "Cos(" + Str(vi1 + (i - 1) * r1) + ") ^ (1/" + Str(vi + (i - 1) * r) + ") + "
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_7(n As Integer, vi As Double, r As Double, vi1 As Double, r1 As Double) As Double
+        Dim i As Integer
+        Dim f As Double
+        f = 0
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f = f - (Math.Sqrt(Math.Sin(vi + (i - 1) * r) / (Serie_Factorial(vi1 + (i - 1) * r1) / 2)))
+            Else
+                f = f + (Math.Sqrt(Math.Sin(vi + (i - 1) * r) / (Serie_Factorial(vi1 + (i - 1) * r1) / 2)))
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie7_Tex(n As Integer, vi As Double, r As Double, vi1 As Double, r1 As Double) As String
+        Dim i As Integer
+        Dim f As String
+        f = " F = "
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f += " - Sqrt(Sin(" + Str(vi + (i - 1) * r) + ")/(" + Str(vi1 + (i - 1) * r1) + "!/2))"
+            Else
+                f += " + Sqrt(Sin(" + Str(vi + (i - 1) * r) + ")/(" + Str(vi1 + (i - 1) * r1) + "!/2))"
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_8(n As Integer, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, m As Double
+        f = 0
+        m = 1000
+        For i = 1 To n
+            t = (Math.PI / (vi + (i - 1) * r)) / Math.Sqrt(m)
+            m = m + i
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie8_Tex(n As Integer, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim m As Double
+        f = " F = "
+        m = 1000
+        For i = 1 To n
+            t = "(PI/" + Str(vi + (i - 1) * r) + ")/Sqrt(" + Str(m) + ") + "
+            m = m + i
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_9(n As Integer, m As Double, d As Double, x As Double) As Double
+        Dim i As Integer
+        Dim f, t As Double
+        f = 0
+        For i = 1 To n
+            t = (x / d) ^ (1 / m)
+            d = d / 2
+            m = m * 2
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie9_Tex(n As Integer, m As Double, d As Double, x As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        f = " F = "
+        For i = 1 To n
+            t = "(x/" + Str(d) + ") ^ (1/" + Str(m) + ") + "
+            d = d / 2
+            m = m * 2
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_10(n As Integer, b As Double, vi As Double, r As Double) As Double
+        Dim i As Integer
+        Dim f, t, e As Double
+        f = 0
+        For i = 1 To n
+            e = vi + (i - 1) * r
+            t = b ^ (1 / e)
+            b = b * 2
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie10_Tex(n As Integer, b As Double, vi As Double, r As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim e As Double
+        f = " F = "
+        For i = 1 To n
+            e = vi + (i - 1) * r
+            t = Str(b) + "^ (1 /" + Str(e) + ") + "
+            b = b * 2
+            f = f + t
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_11(n As Integer, vi As Double, r As Double, x As Double, m As Double) As Double
+        Dim i As Integer
+        Dim f, t, c As Double
+        Dim b As Boolean
+        b = True
+        f = 0
+        c = 0
+        For i = 1 To n
+            c = c + 2
+            m = m - (i - 1)
+            t = (Math.Sin(vi + (i - 1) * r) / ((Serie_Factorial(c) + x) / m)) ^ (1 / c)
+            If b = True Then
+                f = f - t
+            Else
+                f = f + t
+            End If
+            b = Not b
+        Next
+        Return f
+    End Function
+
+    Public Function Serie11_Tex(n As Integer, vi As Double, r As Double, x As Double, m As Double) As String
+        Dim i As Integer
+        Dim f, t As String
+        Dim c As Double
+        Dim b As Boolean
+        b = True
+        f = " F = "
+        c = 0
+        For i = 1 To n
+            c = c + 2
+            m = m - (i - 1)
+            t = "(Sin(" + Str(vi + (i - 1) * r) + ")/(" + Str(c) + "! + x)/" + Str(m) + ")^(1/" + Str(c) + ")"
+            If b = True Then
+                f = f + " - " + t
+            Else
+                f = f + " + " + t
+            End If
+            b = Not b
+        Next
+        Return f
+    End Function
+
+    Private Sub SERIE1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE1ToolStripMenuItem.Click
+        TextBox6.Text = Serie_1(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+        TextBox7.Text = Serie1_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub SERIE2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE2ToolStripMenuItem.Click
+        TextBox6.Text = Serie_2(TextBox1.Text, TextBox2.Text)
+        TextBox7.Text = Serie2_Tex(TextBox1.Text, TextBox2.Text)
+    End Sub
+
+    Private Sub SERIE3ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE3ToolStripMenuItem.Click
+        TextBox6.Text = Serie_3(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+        TextBox7.Text = Serie3_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub SERIE4ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE4ToolStripMenuItem.Click
+        TextBox6.Text = Serie_4(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+        TextBox7.Text = Serie4_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+    End Sub
+
+    Private Sub SERIE5ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE5ToolStripMenuItem.Click
+        TextBox6.Text = Serie_5(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+        TextBox7.Text = Serie5_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub SERIE6ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE6ToolStripMenuItem.Click
+        TextBox6.Text = Serie_6(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        TextBox7.Text = Serie6_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+    End Sub
+
+    Private Sub SERIE7ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE7ToolStripMenuItem.Click
+        TextBox6.Text = Serie_7(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        TextBox7.Text = Serie7_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+    End Sub
+
+    Private Sub SERIE8ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE8ToolStripMenuItem.Click
+        TextBox6.Text = Serie_8(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+        TextBox7.Text = Serie8_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub SERIE9ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE9ToolStripMenuItem.Click
+        TextBox6.Text = Serie_9(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+        TextBox7.Text = Serie9_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+    End Sub
+
+    Private Sub SERIE10ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE10ToolStripMenuItem.Click
+        TextBox6.Text = Serie_10(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+        TextBox7.Text = Serie10_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text)
+    End Sub
+
+    Private Sub SERIE11ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SERIE11ToolStripMenuItem.Click
+        TextBox6.Text = Serie_11(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        TextBox7.Text = Serie11_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+    End Sub
+End Class
