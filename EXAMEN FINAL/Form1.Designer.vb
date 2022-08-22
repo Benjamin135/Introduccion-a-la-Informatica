fd2d3397b0adb69bb372b458f6de159ce10a4b83
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2022-08-22 14:58:44 -0400

    Examen Final

---
+++ EXAMEN FINAL/Form1.Designer.vb
@@ -0,0 +1,181 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.EXAMENFINALToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EXAMENFINALToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA1ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA2ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EXAMENFINALSDYELECTToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA1ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTA2ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.RESPUESTA21ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EXAMENFINALToolStripMenuItem, Me.EXAMENFINALToolStripMenuItem1, Me.EXAMENFINALSDYELECTToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(534, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'EXAMENFINALToolStripMenuItem
+        '
+        Me.EXAMENFINALToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RESPUESTA1ToolStripMenuItem, Me.RESPUESTA2ToolStripMenuItem})
+        Me.EXAMENFINALToolStripMenuItem.Name = "EXAMENFINALToolStripMenuItem"
+        Me.EXAMENFINALToolStripMenuItem.Size = New System.Drawing.Size(117, 20)
+        Me.EXAMENFINALToolStripMenuItem.Text = "EXAMEN FINAL Z5"
+        '
+        'RESPUESTA1ToolStripMenuItem
+        '
+        Me.RESPUESTA1ToolStripMenuItem.Name = "RESPUESTA1ToolStripMenuItem"
+        Me.RESPUESTA1ToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
+        Me.RESPUESTA1ToolStripMenuItem.Text = "RESPUESTA 1"
+        '
+        'RESPUESTA2ToolStripMenuItem
+        '
+        Me.RESPUESTA2ToolStripMenuItem.Name = "RESPUESTA2ToolStripMenuItem"
+        Me.RESPUESTA2ToolStripMenuItem.Size = New System.Drawing.Size(142, 22)
+        Me.RESPUESTA2ToolStripMenuItem.Text = "RESPUESTA 2"
+        '
+        'EXAMENFINALToolStripMenuItem1
+        '
+        Me.EXAMENFINALToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RESPUESTA1ToolStripMenuItem1, Me.RESPUESTA2ToolStripMenuItem1})
+        Me.EXAMENFINALToolStripMenuItem1.Name = "EXAMENFINALToolStripMenuItem1"
+        Me.EXAMENFINALToolStripMenuItem1.Size = New System.Drawing.Size(118, 20)
+        Me.EXAMENFINALToolStripMenuItem1.Text = "EXAMEN FINAL SC"
+        '
+        'RESPUESTA1ToolStripMenuItem1
+        '
+        Me.RESPUESTA1ToolStripMenuItem1.Name = "RESPUESTA1ToolStripMenuItem1"
+        Me.RESPUESTA1ToolStripMenuItem1.Size = New System.Drawing.Size(142, 22)
+        Me.RESPUESTA1ToolStripMenuItem1.Text = "RESPUESTA 1"
+        '
+        'RESPUESTA2ToolStripMenuItem1
+        '
+        Me.RESPUESTA2ToolStripMenuItem1.Name = "RESPUESTA2ToolStripMenuItem1"
+        Me.RESPUESTA2ToolStripMenuItem1.Size = New System.Drawing.Size(142, 22)
+        Me.RESPUESTA2ToolStripMenuItem1.Text = "RESPUESTA 2"
+        '
+        'EXAMENFINALSDYELECTToolStripMenuItem
+        '
+        Me.EXAMENFINALSDYELECTToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RESPUESTA1ToolStripMenuItem2, Me.RESPUESTA2ToolStripMenuItem2, Me.RESPUESTA21ToolStripMenuItem})
+        Me.EXAMENFINALSDYELECTToolStripMenuItem.Name = "EXAMENFINALSDYELECTToolStripMenuItem"
+        Me.EXAMENFINALSDYELECTToolStripMenuItem.Size = New System.Drawing.Size(121, 20)
+        Me.EXAMENFINALSDYELECTToolStripMenuItem.Text = "EXAMEN FINAL SD "
+        '
+        'RESPUESTA1ToolStripMenuItem2
+        '
+        Me.RESPUESTA1ToolStripMenuItem2.Name = "RESPUESTA1ToolStripMenuItem2"
+        Me.RESPUESTA1ToolStripMenuItem2.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTA1ToolStripMenuItem2.Text = "RESPUESTA 1"
+        '
+        'RESPUESTA2ToolStripMenuItem2
+        '
+        Me.RESPUESTA2ToolStripMenuItem2.Name = "RESPUESTA2ToolStripMenuItem2"
+        Me.RESPUESTA2ToolStripMenuItem2.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTA2ToolStripMenuItem2.Text = "RESPUESTA 2"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(93, 95)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(215, 95)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(337, 95)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(93, 158)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(344, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'RESPUESTA21ToolStripMenuItem
+        '
+        Me.RESPUESTA21ToolStripMenuItem.Name = "RESPUESTA21ToolStripMenuItem"
+        Me.RESPUESTA21ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTA21ToolStripMenuItem.Text = "RESPUESTA 1.1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(534, 286)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(550, 325)
+        Me.MinimumSize = New System.Drawing.Size(550, 325)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EXAMENFINALToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RESPUESTA1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RESPUESTA2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents EXAMENFINALToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents RESPUESTA1ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents RESPUESTA2ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents EXAMENFINALSDYELECTToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RESPUESTA1ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents RESPUESTA2ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents RESPUESTA21ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ EXAMEN FINAL/Form1.vb
@@ -0,0 +1,252 @@
+﻿Public Class Form1
+    Public Function VerifPrimo(n As Integer) As Boolean
+        Dim c As Integer
+        Dim b As Boolean
+        If n > 1 Then
+            c = 1
+            Do
+                c = c + 1
+            Loop Until n Mod c = 0
+            b = n = c
+        Else
+            b = False
+        End If
+        Return b
+    End Function
+
+    Public Function SeRepite(n As Integer, d As Integer) As Boolean
+        Dim c, da As Integer
+        While n > 0
+            da = n Mod 10
+            n = n \ 10
+            If (da = d) Then
+                c = c + 1
+            End If
+        End While
+        Return c > 1
+    End Function
+
+    Public Function Ndigs(n As Integer) As Integer
+        Dim nd As Integer
+        nd = 0
+        While n > 0
+            n = n \ 10
+            nd = nd + 1
+        End While
+        Return nd
+    End Function
+
+    Public Function Unir2Numeros(n1 As Integer, n2 As Integer) As Integer
+        Dim nu As Integer
+        nu = n1 * 10 ^ Ndigs(n2) + n2
+        Return nu
+    End Function
+
+    Public Function EliminarDigito(n As Integer, dig As Integer) As Integer
+        Dim d, nu, p As Integer
+        Dim b As Boolean
+        b = True
+        d = 0
+        p = -1
+        While n > 0 And b = True
+            d = n Mod 10
+            n = n \ 10
+            If d <> dig Then
+                p = p + 1
+                nu = nu + d * 10 ^ p
+            Else
+                b = False
+            End If
+        End While
+        nu = Unir2Numeros(n, nu)
+        Return nu
+    End Function
+
+    Public Function Eliminar_Digito(n As Integer, d As Integer) As Integer
+        Dim r, da, m As Integer
+        r = 0
+        m = 1
+        While n > 0
+            da = n Mod 10
+            n = n \ 10
+            If d <> da Then
+                r = r + (da * m)
+                m = m * 10
+            End If
+        End While
+        Return r
+    End Function
+
+    Public Function Invertir(n As Integer) As Integer
+        Dim d, nu As Integer
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            nu = nu * 10 + d
+        End While
+        Return nu
+    End Function
+
+    Public Function VerifFibo(n As Integer) As Boolean
+        Dim a, b, c As Integer
+        a = -1 : b = 1
+        Do
+            c = a + b
+            a = b
+            b = c
+        Loop Until c = n Or c > n
+        Return c = n
+    End Function
+
+    Public Function OrderDigMenorMayor(n As Integer) As Integer
+        Dim d, ne As Integer
+        ne = 0
+        While n > 0
+            d = DigMenor(n)
+            ne = ne * 10 + d
+            n = EliminarDigito(n, d)
+        End While
+        Return ne
+    End Function
+
+    Public Function DigMenor(n As Integer) As Integer
+        Dim d, men As Integer
+        men = n Mod 10 : n = n \ 10
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            If (d < men) Then
+                men = d
+            End If
+        End While
+        Return men
+    End Function
+
+    Public Function Respuesta_1(n As Integer) As Integer
+        Dim d, nu As Integer
+        nu = 0
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            If VerifPrimo(d) Then
+                nu = nu * 10 + d
+                If SeRepite(nu, d) Then
+                    nu = Eliminar_Digito(nu, d)
+                    nu = nu * 10 + d
+                End If
+            End If
+        End While
+        Return Invertir(nu)
+    End Function
+
+    Public Function Respuesta_2(n As Integer, d1 As Integer, d2 As Integer) As Integer
+        Dim d, nu, m As Integer
+        nu = 0 : m = 1           'n = 234581: d1 = 3: d2 = 5
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            If (d = d2) Then
+                nu = nu + (d1 * m)
+                m = m * 10
+            Else
+                If (d = d1) Then
+                    nu = nu + (d2 * m)
+                    m = m * 10
+                Else
+                    nu = nu + (d * m)
+                    m = m * 10
+                End If
+            End If
+        End While
+        Return nu
+    End Function
+
+    Public Function Respuesta_3(n As Integer) As Integer
+        Dim d, nu As Integer
+        nu = 0                          ' n = 6535422
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            If VerifFibo(d) Then
+                nu = nu * 10 + d
+                If SeRepite(nu, d) Then
+                    nu = Eliminar_Digito(nu, d)
+                    nu = nu * 10 + d
+                End If
+            End If
+        End While
+        Return Invertir(nu)
+    End Function
+
+    Public Function Respuesta_4(n As Integer) As Integer
+        Dim d, nu, p As Integer
+        Dim b As Boolean
+        nu = 0 : p = -1                   ' n = 43537
+        b = True
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            If b = True Then
+                p = p + 1
+                nu = nu + d * 10 ^ p
+            End If
+        End While
+        Return nu
+    End Function
+
+    Public Function Respuesta_1_SD(n As Integer, d1 As Integer) As Integer
+        Dim d, aux, p, r As Integer
+        Dim b As Boolean
+        b = True : aux = 0 : p = -1
+        While (n > 0) And (b = True)
+            d = n Mod 10
+            n = n \ 10
+            If d > d1 Then
+                p = p + 1
+                aux = aux + d * 10 ^ p
+            Else
+                b = False
+            End If
+        End While
+        If b = False Then
+            n = n * 10 + d
+            n = n * 10 + d1
+            r = Unir2Numeros(n, aux)
+        Else
+            r = Unir2Numeros(d1, aux)
+        End If
+        Return r
+    End Function
+
+    Public Function Respuesta_5(n As Integer, d1 As Integer) As Integer
+        Return OrderDigMenorMayor(Unir2Numeros(n, d1))
+    End Function
+
+    Private Sub RESPUESTA2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTA2ToolStripMenuItem.Click
+        TextBox4.Text = Respuesta_2(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub RESPUESTA1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTA1ToolStripMenuItem.Click
+        TextBox4.Text = Respuesta_1(TextBox1.Text)
+    End Sub
+
+    Private Sub RESPUESTA1ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles RESPUESTA1ToolStripMenuItem1.Click
+        TextBox4.Text = Respuesta_3(TextBox1.Text)
+    End Sub
+
+    Private Sub RESPUESTA2ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles RESPUESTA2ToolStripMenuItem1.Click
+        TextBox4.Text = Respuesta_4(TextBox1.Text)
+    End Sub
+
+    Private Sub RESPUESTA2ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles RESPUESTA2ToolStripMenuItem2.Click
+        TextBox4.Text = Respuesta_2(TextBox1.Text, TextBox2.Text, TextBox3.Text)
+    End Sub
+
+    Private Sub RESPUESTA1ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles RESPUESTA1ToolStripMenuItem2.Click
+        TextBox4.Text = Respuesta_5(TextBox1.Text, TextBox2.Text)
+    End Sub
+
+    Private Sub RESPUESTA21ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTA21ToolStripMenuItem.Click
+        TextBox4.Text = Respuesta_1_SD(TextBox1.Text, TextBox2.Text)
+    End Sub
+End Class
