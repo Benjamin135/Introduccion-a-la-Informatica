ad1b53c192ffa38279d9b9d4b75253c3cd0baf08
Benjamin Condori Vasquez <benjamincondori7@gmail.com>
2022-08-22 14:50:47 -0400

    Examen de Series

---
+++ EXAMEN DE SERIES/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.EXAMENDESERIESToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RESPUESTAToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.REPUESTA2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EXAMENDESERIESToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(607, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'EXAMENDESERIESToolStripMenuItem
+        '
+        Me.EXAMENDESERIESToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.RESPUESTAToolStripMenuItem, Me.REPUESTA2ToolStripMenuItem})
+        Me.EXAMENDESERIESToolStripMenuItem.Name = "EXAMENDESERIESToolStripMenuItem"
+        Me.EXAMENDESERIESToolStripMenuItem.Size = New System.Drawing.Size(120, 20)
+        Me.EXAMENDESERIESToolStripMenuItem.Text = "EXAMEN DE SERIES"
+        '
+        'RESPUESTAToolStripMenuItem
+        '
+        Me.RESPUESTAToolStripMenuItem.Name = "RESPUESTAToolStripMenuItem"
+        Me.RESPUESTAToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.RESPUESTAToolStripMenuItem.Text = "RESPUESTA"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(33, 107)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(139, 107)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(245, 107)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 3
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(351, 107)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 4
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(462, 107)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox5.TabIndex = 5
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(33, 197)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(345, 20)
+        Me.TextBox6.TabIndex = 6
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(33, 237)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(529, 20)
+        Me.TextBox7.TabIndex = 7
+        '
+        'REPUESTA2ToolStripMenuItem
+        '
+        Me.REPUESTA2ToolStripMenuItem.Name = "REPUESTA2ToolStripMenuItem"
+        Me.REPUESTA2ToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.REPUESTA2ToolStripMenuItem.Text = "REPUESTA 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(607, 360)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(623, 399)
+        Me.MinimumSize = New System.Drawing.Size(623, 399)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EXAMENDESERIESToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RESPUESTAToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents REPUESTA2ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ EXAMEN DE SERIES/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Public Function Serie_Factorial(n As Integer) As Double
+        Dim i As Integer
+        Dim f As Double
+        If (n > 0) Then
+            f = 1
+            For i = n To 1 Step -1
+                f = f * i
+            Next
+        Else
+            f = 1
+        End If
+        Return f
+    End Function
+
+    Public Function Serie_7(n As Integer, vi1 As Double, r1 As Double, vi2 As Double, r2 As Double) As Double
+        Dim i As Integer
+        Dim f As Double
+        f = 0
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f = f - (Math.Sqrt(Math.Sin(vi1 + (i - 1) * r1) / (Serie_Factorial(vi2 + (i - 1) * r2) / 2)))
+            Else
+                f = f + (Math.Sqrt(Math.Sin(vi1 + (i - 1) * r1) / (Serie_Factorial(vi2 + (i - 1) * r2) / 2)))
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie7_Tex(n As Integer, vi1 As Double, r1 As Double, vi2 As Double, r2 As Double) As String
+        Dim i As Integer
+        Dim f As String
+        f = " F = "
+        For i = 1 To n
+            If i Mod 2 = 1 Then
+                f += " - Sqrt(Sin(" + Str(vi1 + (i - 1) * r1) + ")/(" + Str(vi2 + (i - 1) * r2) + "!/2))"
+            Else
+                f += " + Sqrt(Sin(" + Str(vi1 + (i - 1) * r1) + ")/(" + Str(vi2 + (i - 1) * r2) + "!/2))"
+            End If
+        Next
+        Return f
+    End Function
+
+    Public Function Serie_8(n As Integer, vi1 As Double, r1 As Double, vi2 As Double, r2 As Double) As Double
+        Dim i As Integer
+        Dim f As Double
+        Dim bn As Boolean
+        f = 0
+        bn = True
+        For i = 1 To n
+            If bn = True Then
+                f -= Math.Sqrt(Math.Sin(vi1 + (i - 1) * r1) / (Serie_Factorial(vi2 + (i - 1) * r2) / 2))
+            Else
+                f += Math.Sqrt(Math.Sin(vi1 + (i - 1) * r1) / (Serie_Factorial(vi2 + (i - 1) * r2) / 2))
+            End If
+            bn = Not bn
+        Next
+        Return f
+    End Function
+
+    Public Function Serie8_Tex(n As Integer, vi1 As Double, r1 As Double, vi2 As Double, r2 As Double) As String
+        Dim i As Integer
+        Dim f As String
+        Dim bn As Boolean
+        f = " F = "
+        bn = True
+        For i = 1 To n
+            If bn = True Then
+                f += " - Sqrt(Sin(" + Str(vi1 + (i - 1) * r1) + ")/(" + Str(vi2 + (i - 1) * r2) + "!" + "/ 2))"
+            Else
+                f += " + Sqrt(Sin(" + Str(vi1 + (i - 1) * r1) + ")/(" + Str(vi2 + (i - 1) * r2) + "!" + "/ 2))"
+            End If
+            bn = Not bn
+        Next
+        Return f
+    End Function
+
+    Private Sub RESPUESTAToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RESPUESTAToolStripMenuItem.Click
+        TextBox6.Text = Serie_7(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        TextBox7.Text = Serie7_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+    End Sub
+
+    Private Sub REPUESTA2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles REPUESTA2ToolStripMenuItem.Click
+        TextBox6.Text = Serie_8(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+        TextBox7.Text = Serie8_Tex(TextBox1.Text, TextBox2.Text, TextBox3.Text, TextBox4.Text, TextBox5.Text)
+    End Sub
+End Class
